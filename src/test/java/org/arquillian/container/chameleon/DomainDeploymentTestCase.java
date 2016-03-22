85181267ed31b898cf7f7929cfea5296b478391b
Martin Gencur <mgencur@redhat.com>
2016-03-22 17:38:31 +0100

    Support Domain mode of WildFly and EAP servers

---
+++ src/test/java/org/arquillian/container/chameleon/DomainDeploymentTestCase.java
@@ -0,0 +1,56 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.arquillian.container.chameleon;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+@RunWith(Arquillian.class)
+public class DomainDeploymentTestCase {
+
+    @Deployment
+    @TargetsContainer("main-server-group")
+    public static WebArchive deploy() {
+        return ShrinkWrap.create(WebArchive.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addClass(SimpleBean.class);
+    }
+
+    @Inject
+    private SimpleBean bean;
+
+    @Test
+    public void shouldNotBeNull() {
+        Assert.assertNotNull(bean);
+    }
+
+    @Test
+    public void shouldReturnName() {
+        Assert.assertEquals("Proxy", bean.getName());
+    }
+}
