6a957fb06c28624f6c3b64c42bd51ed87e018b65
Bartosz Majsak <bartosz.majsak@gmail.com>
2016-07-06 16:36:09 +0200

    Changes Wildfly configuration to have both addresses exposed under the same IP as InetAddress.getLocalHost() returns. Making tests more portable

--- src/test/java/org/arquillian/container/chameleon/ManualUpdateContainerPropertiesTestCase.java
+++ src/test/java/org/arquillian/container/chameleon/ManualUpdateContainerPropertiesTestCase.java
@@ -1,9 +1,5 @@
 package org.arquillian.container.chameleon;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-
 import org.jboss.arquillian.container.test.api.Config;
 import org.jboss.arquillian.container.test.api.ContainerController;
 import org.jboss.arquillian.junit.Arquillian;
@@ -11,6 +7,11 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Map;
 
 @RunWith(Arquillian.class)
 public class ManualUpdateContainerPropertiesTestCase {
@@ -20,21 +21,34 @@ public class ManualUpdateContainerPropertiesTestCase {
 
     @Test
     public void shouldBeAbleToStartTargetContainerWithNewArguments() throws Exception {
-        // Update configuration from arquillian.xml to bind to a new port
-        cc.start("manual",
-                new Config()
-                    .add("javaVmArguments", "-Djboss.socket.binding.port-offset=1000")
-                    .add("managementPort", "10990").map());
+        // given
+        final InetAddress localHost = InetAddress.getLocalHost();
+        final Integer newPort = 10990;
+        final String jvmArguments = String.format("-Djboss.socket.binding.port-offset=1000 -Djboss.bind.address=%1$s -Djboss.bind.address.management=%1$s", localHost.getHostAddress());
+        final Map<String, String> newContainerConfiguration = new Config()
+                .add("javaVmArguments", jvmArguments)
+                .add("managementPort", newPort.toString())
+                .add("managementAddress", localHost.getHostAddress())
+                .map();
 
+        // when
+        cc.start("manual", newContainerConfiguration);
+
+        // then
+        assertConnectionToManagementConsole(localHost, newPort);
+    }
+
+    private void assertConnectionToManagementConsole(InetAddress localHost, Integer newPort) throws IOException {
         Socket socket = null;
         try {
-            socket = new Socket(InetAddress.getLocalHost(), 10990);
+            socket = new Socket(localHost, newPort);
             Assert.assertTrue(socket.isConnected());
         } finally {
-            if(socket != null) {
+            if (socket != null) {
                 try {
                     socket.close();
-                } catch(IOException ie) {}
+                } catch (IOException ie) {
+                }
             }
         }
     }
