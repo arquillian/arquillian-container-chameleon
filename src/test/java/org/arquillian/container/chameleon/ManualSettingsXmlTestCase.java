be6c7efabc08981062776047da58d2bdf8079798
Matous Jobanek <mjobanek@redhat.com>
2016-11-07 14:57:21 +0100

    [issue #53] Test refactoring: intoduced assertj and NotThreadSafe annotation; added comments

--- src/test/java/org/arquillian/container/chameleon/ManualSettingsXmlTestCase.java
+++ src/test/java/org/arquillian/container/chameleon/ManualSettingsXmlTestCase.java
@@ -6,11 +6,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
+import net.jcip.annotations.NotThreadSafe;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.junit.Assert;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,17 +20,31 @@ import org.junit.runner.RunWith;
 import org.junit.runner.notification.Failure;
 import org.junit.runners.model.InitializationError;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
  */
+@NotThreadSafe
 public class ManualSettingsXmlTestCase {
 
+    // path to a target directory of this project
     private static String target = System.getProperty("user.dir") + "/target/";
+
+    // settings.xml using repository located in src/test/resources/settings-repository/repository
     private static File dummySettingsXml = new File(target + "/test-classes/settings-repository/dummy-settings.xml");
+    // arquillian.xml with two configurations
     private String arqXmlWithSettings = target + "/test-classes/settings-repository/arquillian-with-settings.xml";
-    private File localRepo = new File(target + "/local-repository");
-    private File chameleonDistributionDownloadFolder = new File(target + "/chameleonDistributionDownloadFolder");
-    private File chameleonResolveCacheFolder = new File("/chameleonResolveCacheFolder");
+    // our dummy local repository
+    private static File localRepo = new File(target + "/local-repository");
+
+    // chameleon cache and download folders
+    private static File chameleonDistributionDownloadFolder = new File(target + "/chameleonDistributionDownloadFolder");
+    private static File chameleonResolveCacheFolder = new File("/chameleonResolveCacheFolder");
+
+    // previous properties
+    private static String previousArqXmlProperty = "";
+    private static String previousArqLaunchProperty = "";
 
     @BeforeClass
     public static void prepare() throws IOException {
@@ -37,13 +52,31 @@ public class ManualSettingsXmlTestCase {
         String content = IOUtils.toString(new FileInputStream(dummySettingsXml), "UTF-8");
         content = content.replaceAll("%basedir%", System.getProperty("user.dir"));
         IOUtils.write(content, new FileOutputStream(dummySettingsXml), "UTF-8");
+
+        // store properties
+        previousArqXmlProperty = System.getProperty("arquillian.xml");
+        previousArqLaunchProperty = System.getProperty("arquillian.launch");
     }
 
     @Before
-    public void cleanup() throws IOException {
+    public void cleanBeforeTest() throws IOException {
+        cleanup();
+    }
+
+    public static void cleanup() throws IOException {
         FileUtils.deleteDirectory(localRepo);
         FileUtils.deleteDirectory(chameleonDistributionDownloadFolder);
         FileUtils.deleteDirectory(chameleonResolveCacheFolder);
+    }
+
+    @AfterClass
+    public static void resetPropertiesAndDoCleanup() throws IOException {
+        // set properties to previous values
+        System.setProperty("arquillian.xml", previousArqXmlProperty == null ? "" : previousArqXmlProperty);
+        System.setProperty("arquillian.launch", previousArqLaunchProperty == null ? "" : previousArqLaunchProperty);
+
+        // remove created directories
+        cleanup();
     }
 
     @Test
@@ -60,14 +93,13 @@ public class ManualSettingsXmlTestCase {
             message.append("\nThe stacktrace:\n" + failures.get(0).getTrace());
         }
         // it should have not failed
-        Assert.assertTrue(message.toString(), failures.isEmpty());
+        assertThat(failures).as(message.toString()).isEmpty();
 
         // verify that target/local-repository was used
-        Assert.assertTrue("target/local-repository directory should exist", localRepo.exists());
-        Assert.assertTrue("target/local-repository directory should be a directory", localRepo.isDirectory());
-        Assert.assertTrue("target/local-repository directory should not be empty", localRepo.listFiles().length > 0);
-        Assert.assertTrue("target/local-repository directory should contain org directory",
-                          new File(localRepo + "/org").exists());
+        assertThat(localRepo).exists();
+        assertThat(localRepo).isDirectory();
+        assertThat(localRepo.listFiles()).as("target/local-repository directory should not be empty").isNotEmpty();
+        assertThat(new File(localRepo + "/org")).exists();
     }
 
     @Test
@@ -79,8 +111,8 @@ public class ManualSettingsXmlTestCase {
         // run test
         List<Failure> failures = JUnitCore.runClasses(TestClass.class).getFailures();
         // it should have failed
-        Assert.assertFalse("The test should have failed but it didn't", failures.isEmpty());
-        Assert.assertFalse("target/local-repository directory should NOT exist", localRepo.exists());
+        assertThat(failures).as("The test should have failed but it didn't").isNotEmpty();
+        assertThat(localRepo).doesNotExist();
     }
 
     @RunWith(Arquillian.class)
