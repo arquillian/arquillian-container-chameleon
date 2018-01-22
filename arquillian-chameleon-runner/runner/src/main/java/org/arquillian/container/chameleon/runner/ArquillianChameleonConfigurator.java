2e6aeefe519d35fc70b62fc988b0247bfb908ed7
Alex Soto <asotobu@gmail.com>
2018-01-22 15:24:31 +0100

    chore: Adds warning in case of parallel tests

--- arquillian-chameleon-runner/runner/src/main/java/org/arquillian/container/chameleon/runner/ArquillianChameleonConfigurator.java
+++ arquillian-chameleon-runner/runner/src/main/java/org/arquillian/container/chameleon/runner/ArquillianChameleonConfigurator.java
@@ -3,11 +3,12 @@ package org.arquillian.container.chameleon.runner;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 import javax.xml.transform.TransformerException;
 
 public class ArquillianChameleonConfigurator {
 
-    
+    private static final Logger log = Logger.getLogger(ArquillianChameleonConfigurator.class.getName());
 
     public Path setup(Class<?> testClass, ClassLoader parent) throws IOException, TransformerException {
 
@@ -28,6 +29,9 @@ public class ArquillianChameleonConfigurator {
 
                 arquillianChameleonConfiguration = arquillianChameleonConfigurationGenerator.generateAppendedArquillianXml(testClass, configurationXml);
                 System.setProperty("arquillian.xml", arquillianChameleonConfiguration.getFileName().toString());
+
+                log.warning("Current project is configured with arquillian.properties and arquillian.xml. So we have created a custom arquillian filename and set it using a JVM System property."
+                    + "If you are planning to run tests in parallel in same JVM, this might cause some problems. We recommend to configure Arquillian either with .proeprties or .xml file approach but not both.");
 
             } else {
                 arquillianChameleonConfiguration =
